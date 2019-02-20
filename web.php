79313e8b9628464b35feb52bdd091c7f445d2084
Bradltr95 <47356514+Bradltr95@users.noreply.github.com>
2019-02-19 21:39:10 -0500

    Update web.php
    Updated the web.php file to be more readable

--- web.php
+++ web.php
@@ -13,56 +13,50 @@ use App\Post;
 |
 */
 
+// Laravel welcome route 
 Route::get('/', function() {
     return view('welcome');
 });
 
 // route to the index function in PostsController
 // when the users is on the /posts page
-//Route::get('/posts', 'PostsController@index');
+// Route::get('/posts', 'PostsController@index');
 
-/* Naming a route using an array, we can store that into
-   a $url variable for use later
+// Naming a route using an array, we can store that into
+// a $url variable for use later 
 Route::get('/admin/home/page/34', array('as'=>'admin.home' ,function() {
     $url = route('admin.home');
 
     return "This url is " . $url;
 }));
-*/
 
 /*-------------------------------------------------------------
 | Create a route that will insert data into the database.
-|--------------------------------------------------------------
-*/
-/*
+|--------------------------------------------------------------*/
 Route::get('/insert', function(){
     DB::insert('insert into posts(title, content) values(?, ?)', ['PHP', 'Testing PHP']);
 });
-*/
+
 /*-------------------------------------------------------------
 | Create a route that will read the data from our database.
-|--------------------------------------------------------------
-*/
-/*
+|--------------------------------------------------------------*/
 Route::get('/read', function(){
     // returns STD class object (Generic Empty Class, like dynamic properties).
     $results = DB::select('select * from posts where id = ?', [1]);
-    */
+    
     /*
     foreach($results as $post) {
         return $post->title;
     }
     */
-    /*
+    
     // we can also use var_dump() for associative array
     return var_dump($results);
 });
-*/
+
 /*-------------------------------------------------------------
 | Create a route that will update data in our database.
-|--------------------------------------------------------------
-*/
-/*
+|--------------------------------------------------------------*/
 Route::get('/update', function(){
     $updated = DB::update('update posts set title = "Updated title" where id = ?', [1]);
 
@@ -72,12 +66,9 @@ Route::get('/update', function(){
         print("Record Updated!");
     }
 });
-*/
 /*-------------------------------------------------------------
 | Create a route that will delete data from our database.
-|--------------------------------------------------------------
-*/
-/*
+|--------------------------------------------------------------*/
 Route::get('/delete', function(){
    $deleted = DB::delete('delete from posts where id = ?', [1]);
 
@@ -87,7 +78,6 @@ Route::get('/delete', function(){
        print("Record Deleted!");
    }
 });
-*/
 
 //Route::resource('posts', 'PostsController');
 
@@ -98,35 +88,29 @@ Route::get('/delete', function(){
 /*-------------------------------------------------------------
 | Learning ELOQUENT
 |--------------------------------------------------------------
-*/
-
-/*-------------------------------------------------------------
 | READING data with ELOQUENT - read
-|--------------------------------------------------------------
-*/
-//Route::get('/read', function(){
-    //$posts = App\Post;
-    //$posts = Post::all();
+|--------------------------------------------------------------*/
+Route::get('/read', function(){
+    $posts = App\Post;
+    $posts = Post::all();
 
-    //foreach($posts as $post) {
-    //    return $post->title;
-    //}
-//});
-/*
+    foreach($posts as $post) {
+        return $post->title;
+    }
+});
+
 Route::get('/find', function(){
     $post = Post::find(4);
 
     return $post->title;
 });
-*/
-/*
+
 Route::get('/findwhere', function(){
    $posts = Post::where('id', 5)->orderBy('id', 'desc')->take(1)->get();
 
    return $posts;
 });
-*/
-/*
+
 Route::get('/findmore', function(){
     //$posts = Post::findOrFail(4);
 
@@ -137,13 +121,11 @@ Route::get('/findmore', function(){
     //print($posts['title']);
     //return $posts;
 });
-*/
+
 /*-------------------------------------------------------------
 | INSERTING data with ELOQUENT - insert
-|--------------------------------------------------------------
-*/
-/*
- * // INSERTING NEW DATA
+|--------------------------------------------------------------*/
+// INSERTING NEW DATA
 Route::get('/basicinsert', function(){
     // Crate the Post object
     $post = new Post;
@@ -155,9 +137,8 @@ Route::get('/basicinsert', function(){
     // Insert a new record to the database.
     $post->save();
 });
-*/
+
 // UPDATING DATA IN THE
-/*
 Route::get('/basicinsert2', function(){
     // Find a specific post ID
     $post = Post::find(4);
@@ -169,39 +150,34 @@ Route::get('/basicinsert2', function(){
     // Update the record from our database.
     $post->save();
 });
-*/
+
 /*-------------------------------------------------------------
 | CREATING data with ELOQUENT - create
-|--------------------------------------------------------------
-*/
-/*
+|--------------------------------------------------------------*/
 Route::get('/create', function(){
     Post::create(['title'=>'the create method!', 'content'=>'WOW IM LEARNING SO MUCH WITH LARAVEL']);
 });
-*/
+
 /*-------------------------------------------------------------
 | UPDATING data with ELOQUENT - update
-|--------------------------------------------------------------
-*/
-/*
+|--------------------------------------------------------------*/
 Route::get('/update', function() {
    Post::where('id', 5)->where('is_admin', 0)->update(
        ['title'=>'NEW PHP TITLE', 'content'=>'NEW CONTENT FROM ELOQUENT']
    );
 });
-*/
+
 /*-------------------------------------------------------------
 | DELETING data with ELOQUENT - delete
-|--------------------------------------------------------------
-*/
-/* // DELETE SINGLE USING STATIC FIND FUNCTION
+|--------------------------------------------------------------*/
+// DELETE SINGLE USING STATIC FIND FUNCTION
 Route::get('/delete', function(){
     $post = Post::find(4);
 
     $post->delete();
 });
-*/ // DESTROY MULTI OR SPECIFIC
-/*
+
+// DESTROY MULTI OR SPECIFIC
 Route::get('/delete2', function(){
     // destroy multi
     Post::destroy[5,6,7];
@@ -209,16 +185,13 @@ Route::get('/delete2', function(){
     // destroy single
     Post::destroy(4);
 
-    delete data using static where function
+    //delete data using static ::where() function
     Post::where('is_admin', 0)->delete();
 });
-*/
+
 /*-------------------------------------------------------------
 | SOFT DELETING/TRASHING data with ELOQUENT - soft delete
-|--------------------------------------------------------------
-*/
-/*
+|--------------------------------------------------------------*/
 Route::get('/softdelete', function(){
     Post::find(5)->delete();
 });
-*/
